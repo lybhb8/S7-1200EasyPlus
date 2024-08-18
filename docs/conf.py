92eee67dc602068aa3e11c0ec61126de14515814
lybhb8 <lybhb8@gmail.com>
2024-08-19 00:40:36 +0800

    test-furo

--- docs/conf.py
+++ docs/conf.py
@@ -1,38 +1,48 @@
-# 头部添加导入
-# import sphinx_book_theme
-
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# Full list of options can be found in the Sphinx documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+from typing import Any, Dict
 
-project = 'S7-1200 PLC 技术手册 EasyPlus'
-copyright = '2024, 宁波双紫信息科技有限公司'
-author = 'lybhb8'
-release = '4.3'
+# add the demo python code to the path, so that it can be used to demonstrate
+# source links
+sys.path.append(os.path.abspath("./kitchen-sink/demo_py"))
+
+# -- Project information -----------------------------------------------------
+#
+
+project = "Smart20"
+copyright = "2023, Bobolin"
+author = "lybhb8"
+release = '2.00'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-import os, sys; sys.path.append(os.path.abspath('sphinxext'))
+#
 
 extensions = [
-    #"sphinx.ext.todo",
-    #"sphinx.ext.intersphinx",
-    #"sphinx.ext.autosectionlabel",
+    # Sphinx's own extensions
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    # Our custom extension,# only meant for Furo's own documentation.
+    "furo.sphinxext",
+    # External stuff
     "myst_parser",
-    #"sphinx_design",
-    #"sphinx_copybutton",
-    #"sphinx_togglebutton",
-    #'extname',
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_inline_tabs",
+    "sphinx.ext.autosectionlabel",
+    
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
-language = 'zh_CN'
+# 内联标签
+autosectionlabel_prefix_document = True
 
 source_suffix = {
     
@@ -45,20 +55,121 @@ source_suffix = {
 source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
 
 
+
+# -- Options for extlinks ----------------------------------------------------
+#
+
+extlinks = {
+    "pypi": ("https://pypi.org/project/%s/", "%s"),
+}
+
+# -- Options for intersphinx -------------------------------------------------
+#
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
+}
+
+# -- Options for TODOs -------------------------------------------------------
+#
+
+todo_include_todos = True
+
+# -- Options for Markdown files ----------------------------------------------
+#
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+myst_heading_anchors = 3
+
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+#
 
-# html_theme = 'press'
-html_theme = 'sphinx_rtd_theme'
+html_theme = "furo"
+html_title = "Smart20控制系统"
+language = "zh_CN"
 
+html_static_path = ["_static"]
+html_css_files = ["pied-piper-admonition.css"]
 
-html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
+html_theme_options: Dict[str, Any] = {
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/lybhb8/Smart20-Docs",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+    "source_repository": "https://github.com/lybhb8/Smart20-Docs",
+    "source_branch": "furo",
+    "source_directory": "docs/",
+    "sidebar_hide_name": False,
+    "navigation_with_keys": True,
+    "globaltoc_includehidden": True,
+}
 
+#if "READTHEDOCS" in os.environ:
+#    html_theme_options["announcement"] = (
+#        "This documentation is hosted on Read the Docs only for testing. Please use "
+#        "<a href='https://pradyunsg.me/furo/'>the main documentation</a> instead."
+#    )
 
-# 添加你自己的 CSS 规则
-html_static_path = ['_static']
-html_css_files = ["custom.css"]
-# 自定义徽标、和网站图标
-html_logo = "_static/logo.png"
+# -- Options for theme development -------------------------------------------
+# Make sure these are all set to the default values.
+
+html_js_files = []
+html_context: Dict[str, Any] = {}
+# html_show_sphinx = False
+# html_show_copyright = False
+# html_last_updated_fmt = ""
+
+RTD_TESTING = False
+if RTD_TESTING or "FURO_RTD_TESTING" in os.environ:
+    #del html_theme_options["footer_icons"]
+
+    html_css_files += [
+        "https://assets.readthedocs.org/static/css/readthedocs-doc-embed.css",
+        "https://assets.readthedocs.org/static/css/badge_only.css",
+    ]
+    html_js_files += [
+        "readthedocs-dummy.js",
+        "https://assets.readthedocs.org/static/javascript/readthedocs-doc-embed.js",
+    ]
+    html_context["READTHEDOCS"] = True
+    html_context["current_version"] = "latest"
+    html_context["conf_py_path"] = "/docs/"
+    html_context["display_github"] = True
+    html_context["github_user"] = "lybhb8"
+    html_context["github_repo"] = "Smart20-docs"
+    html_context["github_version"] = "furo"
+    html_context["slug"] = "furo"
+
+FONT_AWESOME_TESTING = False
+if FONT_AWESOME_TESTING:
+    html_css_files += [
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/solid.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css",
+    ]
+
+    html_theme_options["footer_icons"] = [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/lybhb8/Smart20-Docs",
+            "html": "",
+            "class": "fa-brands fa-solid fa-github fa-2x",
+        },
+    ]
+
+#html_logo = "_static/notebook-badge.svg"
+#html_logo = "_static/Boquilla-variable.gif"
 html_favicon = "_static/favicon.ico"
 

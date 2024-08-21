ef574a4d5ca61b33b650930f993be44c804e1b77
lybhb8 <lybhb8@gmail.com>
2024-08-21 09:28:12 +0800

    sphinx+

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ version = re.search("[0-9]\.[0-9]", release).group(0)
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh_CN'
+language = 'zh'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
